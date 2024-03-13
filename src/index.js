fc035dbb45201dcc3bb9b20731cc4c42cb848a06
Munna Ahmed <munnaahmed2025@gmail.com>
2024-03-14 00:47:14 +0600

    sass with parcel

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
+// import "./styles/index.scss";
+
 console.log("is working?...");
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+const rock = new Person("Rock", 57);
+const tom = new Person("Tom", 20);
 
 if (module.hot) {
   module.hot.accept();

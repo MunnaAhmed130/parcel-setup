d85c9d8f0e5d0a5660a19a8539ac17da3b47081a
Munna Ahmed <munnaahmed2025@gmail.com>
2024-03-15 02:01:34 +0600

    add sass browserlist css modules

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 // import "./styles/index.scss";
+import * as styles from "./styles/styles.module.css";
+
+const h2 = document.querySelector("h2");
+h2.classList.add(styles.heading);
+console.log(h2);
 
 console.log("is working?...");
+// console.log("is working?...");
+// console.log("is working?...");
 
 class Person {
   constructor(name, age) {
@@ -8,9 +15,10 @@ class Person {
     this.age = age;
   }
 }
-
 const rock = new Person("Rock", 57);
 const tom = new Person("Tom", 20);
+console.log(rock, tom);
+console.log(rock?.age?.num);
 
 if (module.hot) {
   module.hot.accept();

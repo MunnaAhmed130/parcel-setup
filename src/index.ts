1978d2ada64d319ab5c695989ae6377a86dbe172
Munna Ahmed <munnaahmed2025@gmail.com>
2024-03-15 21:18:22 +0600

    add typescript

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 // import "./styles/index.scss";
-import * as styles from "./styles/styles.module.css";
+// import * as styles from "./styles/styles.module.css";
+import styles from "./styles/styles.module.css";
 
-const h2 = document.querySelector("h2");
+const h2 = document.querySelector("h2") as HTMLElement;
 h2.classList.add(styles.heading);
 // console.log(h2);
 
@@ -10,14 +11,17 @@ console.log("is working?...");
 // console.log("is working?...");
 
 class Person {
-  constructor(name, age) {
+  name: string;
+  age: number;
+  constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
 }
 const rock = new Person("Rock", 57);
 const tom = new Person("Tom", 20);
-// console.log(rock, tom);
+// console.log(rock, tom) ;
+
 // console.log(rock?.age?.num);
 
 if (module.hot) {

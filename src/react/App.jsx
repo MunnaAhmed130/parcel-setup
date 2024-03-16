bd0410ccbf973a474a1bca7ca815d17d4ad1673d
Munna Ahmed <munnaahmed2025@gmail.com>
2024-03-16 15:53:37 +0600

    add styled components

--- src/react/App.jsx
+++ src/react/App.jsx
@@ -1,15 +1,22 @@
 import React, { useState } from "react";
 import swcLogo from "../images/swc-logo.jpg";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+`;
 
 const App = () => {
   // console.log("hello");
   const [state, setState] = useState(false);
   console.log(state);
   return (
-    <div onClick={() => setState(!state)}>
+    <Container onClick={() => setState(!state)}>
       <h2>State is - {state.toString()}</h2>
       <img src={swcLogo} alt="" />
-    </div>
+    </Container>
   );
 };
 
